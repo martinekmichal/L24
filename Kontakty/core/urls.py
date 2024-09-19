6ae0cc9be8fc04ea4602c7c13a896c1529ac558f
martinekmichal <martinek.m.84@gmail.com>
2024-09-19 18:31:37 +0200

    setup + header

---
+++ Kontakty/core/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import *
+from django.contrib.auth import views as kontakty
+
+
+urlpatterns = [
+    path("", home_page, name="home"),
+    path("about/", about_page, name="about"),
+    path("contacts/", contacts_page, name="contacts"),
+    #path("profile/", profile_page, name="profile"),
+    #path('register/', register_page, name='register'),
+    #path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    #path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+]
