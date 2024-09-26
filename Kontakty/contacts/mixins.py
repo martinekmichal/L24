c53a938c04f6bda632caadb3ac77307f6f5bd169
martinekmichal <martinek.m.84@gmail.com>
2024-09-26 19:46:17 +0200

    update logovani mixin do file

--- Kontakty/contacts/mixins.py
+++ Kontakty/contacts/mixins.py
@@ -4,4 +4,7 @@ from datetime import datetime
 
 class LoggingMixin:
     def log_action(self, action):
-        print(f"{action} action performed at {datetime.now()}")
+        #print(f"{action} action performed at {datetime.now()}")
+        with open('media/file/test.txt', 'a', ) as f:
+            f.write(f"{action} action performed at {datetime.now()}\n")
+            
